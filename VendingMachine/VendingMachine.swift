ed3093df412ae51b2dc852a9a52b275e74e5e7e2
Smitha Kolan <smitkolan@gmail.com>
2019-07-26 21:17:43 +0200

    first commit

---
+++ VendingMachine/VendingMachine.swift
@@ -0,0 +1,103 @@
+//
+//  VendingMachine.swift
+//  VendingMachine
+//
+//  Created by Screencast on 12/6/16.
+//  Copyright © 2016 Treehouse Island, Inc. All rights reserved.
+//
+
+import Foundation
+
+enum VendingSelection {
+    case soda
+    case dietSoda
+    case chips
+    case cookie
+    case sandwich
+    case wrap
+    case candyBar
+    case popTart
+    case water
+    case fruitJuice
+    case sportsDrink
+    case gum
+}
+
+protocol VendingItem {
+    var price: Double { get }
+    var quantity: Int { get set }
+}
+
+protocol VendingMachine {
+    var selection: [VendingSelection] { get }
+    var inventory: [VendingSelection: VendingItem] { get set }
+    var amountDeposited: Double { get set }
+    
+    init(inventory: [VendingSelection: VendingItem])
+    func vend(_ quantity: Int, _ selection: VendingSelection) throws
+    func deposit(_ amount: Double)
+}
+
+struct Item: VendingItem {
+    let price: Double
+    var quantity: Int
+}
+
+class FoodVendingMachine: VendingMachine {
+    let selection: [VendingSelection] = [.soda, .dietSoda, .chips, .cookie, .wrap, .sandwich, .candyBar, .popTart, .water, .fruitJuice, .sportsDrink, .gum]
+    var inventory: [VendingSelection : VendingItem]
+    var amountDeposited: Double = 10.0
+    
+    required init(inventory: [VendingSelection : VendingItem]) {
+        self.inventory = inventory
+    }
+    
+    func vend(_ quantity: Int, _ selection: VendingSelection) throws {
+    }
+    
+    func deposit(_ amount: Double) {
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
